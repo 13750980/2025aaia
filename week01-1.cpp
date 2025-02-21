c9050d4db3c6a43d257be8d5caad1417cbf3cf24
13750980 <13750980@me.mcu.edu.tw>
2025-02-21 12:00:25 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,14 @@
+/// week01-1.cpp
+/// 在瘋狂程設(練習模式)寫好的程式碼, 全選複製
+/// File - New-Empty 空白檔案, 存檔
+#include <stdio.h> /// use the standard input/output
+
+int main()
+{
+	int a, b; /// 整數 a,b
+
+	scanf("%d%d", &a, &b ); /// 讀入資料(小心"%d%d" 後面)
+    /// 兩個%d%d  &   &
+	printf("%d", a + b); /// 印出資料, 把答案印出
+
+}
