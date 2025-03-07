5a19ff3474cd254de2cadd3741e8ef236bc1803c
13750980 <13750980@me.mcu.edu.tw>
2025-03-07 12:18:23 +0800

    Delete week03 directory

--- week03/Week03-1.cpp
+++
@@ -1,8 +0,0 @@
-// Week03-1.cpp
-class Solution {
-public:
-    long long coloredCells(int n) {
-        long long ans = n;
-        return ans*ans + (ans-1)*(ans-1); //C/C++/JAVA/C#都要加分號
-    }
-};
